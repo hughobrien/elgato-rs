2cfbcc474c35d5d2db8dc89d42eb8770a218f0b0
Hugh O'Brien <github@hughobrien.ie>
2024-11-11 20:20:02 -0500

    inc

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ struct Light {
 }
 
 #[derive(Deserialize, Serialize)]
+#[allow(non_snake_case)]
 struct LightsResponse {
     numberOfLights: u8,
     lights: Vec<Light>,
@@ -17,19 +18,24 @@ struct LightsResponse {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let (url, direction) = parse_args(env::args().collect())?;
-    
+    let (url, command) = parse_args(env::args().collect())?;
+
     let client = Client::new();
-    let lights_response = client.get(&url).send().await?.json::<LightsResponse>().await?;
-    
+    let lights_response = client
+        .get(&url)
+        .send()
+        .await?
+        .json::<LightsResponse>()
+        .await?;
+
     if let Some(mut light) = lights_response.lights.first().cloned() {
-        adjust_brightness(&mut light, direction);
-        
+        adjust_light(&mut light, command);
+
         let req_body = LightsResponse {
             numberOfLights: lights_response.numberOfLights,
             lights: vec![light],
         };
-        
+
         client.put(&url).json(&req_body).send().await?;
     } else {
         eprintln!("No lights found");
@@ -41,24 +47,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn parse_args(args: Vec<String>) -> Result<(String, &'static str), &'static str> {
     if args.len() == 3 {
         let url = args[1].clone();
-        let direction = match args[2].as_str() {
-            "up" => "up",
-            "down" => "down",
-            _ => return Err("Usage: <program> <url> <up|down>"),
+        let command = match args[2].as_str() {
+            "bright" => "bright",
+            "dim" => "dim",
+            "warm" => "warm",
+            "cold" => "cold",
+            _ => return Err("Usage: <program> <url> <bright|dim|warm|cold>"),
         };
-        Ok((url, direction))
+        Ok((url, command))
     } else {
-        Err("Usage: <program> <url> <up|down>")
+        Err("Usage: <program> <url> <bright|dim|warm|cold>")
     }
 }
 
-fn adjust_brightness(light: &mut Light, direction: &str) {
-    const STEP: u8 = 3;
-    match direction {
-        "up" => light.brightness = light.brightness.saturating_add(STEP).min(100),
-        "down" => light.brightness = light.brightness.saturating_sub(STEP).max(0),
+fn adjust_light(light: &mut Light, command: &str) {
+    const BRIGHTNESS_STEP: u8 = 3;
+    const TEMPERATURE_STEP: u16 = 10;
+
+    match command {
+        "bright" => light.brightness = light.brightness.saturating_add(BRIGHTNESS_STEP).min(100),
+        "dim" => light.brightness = light.brightness.saturating_sub(BRIGHTNESS_STEP).max(0),
+        "warm" => light.temperature = light.temperature.saturating_add(TEMPERATURE_STEP).min(344),
+        "cold" => light.temperature = light.temperature.saturating_sub(TEMPERATURE_STEP).max(143),
         _ => (),
     }
+
     light.on = if light.brightness > 0 { 1 } else { 0 };
 }
-
